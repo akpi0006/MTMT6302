35c5c8d46c615219f0c50c5b2e4fb04e0dc22a77
akpi0006 <122479222+akpi0006@users.noreply.github.com>
2024-04-11 12:00:06 -0400

    close to done

--- script.js
+++ script.js
@@ -25,19 +25,18 @@ const questionTemplate = `
             <p>${questions[0].question}</p>
             <br>
 
-            <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
             <div class="qoptions">
             <li>
                 <input type="checkbox" class="ans" data-answ="answer_a" name="question1" value="var">${questions[0].answers.answer_a}
             </li>
             <li>
-                <input type="checkbox" name="question1" value="let">${questions[0].answers.answer_b}
+                <input type="checkbox" class="ans" data-answ="answer_b" name="question1" value="let">${questions[0].answers.answer_b}
             </li>
             <li>
-                <input type="checkbox" name="question1" value="const">${questions[0].answers.answer_c}
+                <input type="checkbox" class="ans" data-answ="answer_c" name="question1" value="const">${questions[0].answers.answer_c}
             </li>
             <li>
-                <input type="checkbox" name="question1" value="variable"> ${questions[0].answers.answer_d}
+                <input type="checkbox" class="ans" data-answ="answer_d" name="question1" value="variable"> ${questions[0].answers.answer_d}
             </li>
       
 
@@ -58,7 +57,7 @@ const questionTemplate = `
   </section>
 
   <div class="sa">
-            <button class="submit"><a href="hard-answers.html">Submit Answers</a></button>
+            <button class="submit">Submit Answers</a></button>
         </div>`
 
   //use innerHTML to display the data in the container
@@ -69,7 +68,7 @@ container.innerHTML = questionTemplate;
 
 /*** async / await */
 async function getQuestions(difficulty){
-    const response =  await fetch(`https://quizapi.io/api/v1/questions?apiKey=fIzlzqCAAPKQnXiToNo61XYMfZmRhDdhEseZCtMj&limit=5&difficulty=${difficulty}`)
+    const response =  await fetch(`https://quizapi.io/api/v1/questions?apiKey=fIzlzqCAAPKQnXiToNo61XYMfZmRhDdhEseZCtMj&limit=10&difficulty=${difficulty}`)
     const question = await response.json()
     console.log(question)
     questions = question
@@ -80,10 +79,10 @@ async function getQuestions(difficulty){
 //define the function to display the next question using event listener
 
 /** Define the event listener for the click events */
-container.addEventListener('click', function(e){
+    container.addEventListener('click', function(e){
     e.preventDefault()
    
-  if(e.target.classList.contains('pickone')){
+    if(e.target.classList.contains('pickone')){
     getQuestions(e.target.dataset.difficulty)
     
     }
@@ -93,22 +92,95 @@ container.addEventListener('click', function(e){
     }
     else if(e.target.classList.contains('ans')){
         console.log(e.target.dataset.answ)
-        // getAns(e.target.dataset.answ)
-        //1. save answers, question
-        //2. display the next question
+        const selectedAnswer = e.target.dataset.answ;
+        const selectedQuestion = questions[0].question;
+        saveAnswer(selectedQuestion, selectedAnswer);
+        // Proceed to the next question
+    }
+    else if(e.target.classList.contains('submit')){
+        displayResults()
+    }
+    else if(e.target.classList.contains('retake')){
+        buildHome()
+    }
+    //1. save answers, question in local storage
+    function saveAnswer(question, answer) {
+        let savedAnswers = JSON.parse(localStorage.getItem('savedAnswers')) || {};
+        savedAnswers[question] = answer;
+        localStorage.setItem('savedAnswers', JSON.stringify(savedAnswers));
+    }  
+
+    //2. display the next question
+    function displayNextQuestion() {
+       const nextQuestion = {}; 
+        buildQuestion(nextQuestion);
+    }
+    //3. display the results
+        function displayResults() {
+        const savedAnswers = JSON.parse(localStorage.getItem('savedAnswers'));
+        let correctAnswersCount = 0;
+        for (let question in savedAnswers) {
+            if (savedAnswers[question] === questions[0].correct_answer) {
+                correctAnswersCount++;
+            }
         }
+        container.innerHTML = `
+        <div class="flex-container">
+            <section class="act">
+    <div class="start">
+         
+        <div class="gradebox">
+            <div>
+                <span>
+                    <div class="gradeline">Grade</div>
+                    <br>
+                     
+                     You correctly answered ${correctAnswersCount} out of ${questions.length} questions
+                  </span>
+            </div>
+        </div>
 
+</div>
+
+<div class="sa">
+    <button class="retake">Retake Quiz</a></button>
+</div>
+
+`
+
+  }
 })
+  
+function buildHome(question){
+    const homeTemplate =` 
+    <div class="begin">
 
+            <div class="intro">
+                <p>Welcome to <strong>Quiz Me!</strong></ps>
+                    <p>
+                        The ultimate trivia challenge that will <br>
+                        test your knowledge across various categories   and <br> keep you entertained <br> with fun and engaging questions!
+                    </p> 
+                </div>
+    </div>
+<br>
+    <div class="start">
+        <p>Pick your quiz difficulty and dive <br> into the perfect challenge !!</p>
+        <br>
+      
+        <div class="options">
+            <h3 class="pickone pickonebtn" data-difficulty="easy" >
+             Easy
+            </h3>
+            <h3 class="pickone pickonebtn " data-difficulty="medium">
+              Intermediate
+            </h3>
+            <h3 class="pickone pickonebtn" data-difficulty="hard">
+              Hard
+            </h3>
+        </div>
+    </div>
+`
+container.innerHTML =homeTemplate;
 
-//show only 10 questions and enable the user to select only one answer per question
-
-// function displayResults() {
-//     quizContainer.classList.add('hidden');
-//     navigation.classList.add('hidden');
-//     resultsDiv.classList.remove('hidden');
-//     resultsDiv.innerHTML = <p>You correctly answered ${correctAnswersCount} out of ${questions.length} questions.</p>;
-// }
-
-
-//track the number of correct and incorrect answers and store them in local storage. You need to implement this functionality. You can use localStorage.setItem() to store values and localStorage.getItem() to retrieve them.
+}
